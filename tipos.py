761c90ef0d644c5503e84b6d83650c354cd3939e
Gabrielpiolifernandes-afk <gabrielpiolifernandes@gmail.com>
2025-08-29 23:03:43 -0300

    tipos

---
+++ tipos.py
@@ -0,0 +1,22 @@
+#todo texto tem que estar dentro de aspas 
+#o resto so pode ser numeros ou True ou False
+var= 1
+var2= 10.4
+var= 15.2
+
+banana= 10
+
+nome= "gabriel"
+numero = "10"
+
+resultado01 = nome + numero
+print(resultado01) 
+
+booleano = True 
+booleano = False 
+
+lista = ["macarrao","pao","arroz", "molho"]
+lista2= [1,2,3,4]
+
+resultado = var + var2
+print(resultado)
