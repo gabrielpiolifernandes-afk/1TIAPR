dabb8c082f7864173faede9fc7fdd84a9fa85d7a
Gabrielpiolifernandes-afk <gabrielpiolifernandes@gmail.com>
2025-09-19 22:28:49 -0300

    das

---
+++ aula4/ex01.py
@@ -0,0 +1,7 @@
+lista = [10,9,8,7,6,5,4,3,2,1]
+for a in lista:
+    if a > 1:
+        print(a)
+    else:
+        print("fogo!")
+
---
+++ aula4/ex02.py
@@ -0,0 +1,7 @@
+usuarios=int(input("Por favor, digite um numero par:"))
+
+for a in range(2,1000,2):
+    if usuarios == a:
+        print(f"voce passou com" {+usuarios+} "!")
+else:
+    print(f"voce não passou com"{+usuarios+} ")=")
